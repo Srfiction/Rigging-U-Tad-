a918d0b7b5b1c29934b31ae60e09608ced440c45
Josie <49275706+Srfiction@users.noreply.github.com>
2019-06-01 14:47:39 +0200

    Update OffsetMaker.py

--- OffsetMaker.py
+++ OffsetMaker.py
@@ -1,7 +1,9 @@
 list = cmds.ls(sl=True)
 
 for ctr in list:
+    prnt = cmds.listRelatives(ctr, p=True)[0]
     grp = cmds.group(em=True, n='{}_zero'.format(ctr))
     pos = cmds.xform(ctr, matrix=True, q=True, ws=True)
     cmds.xform(grp, matrix=pos, ws=True)
     cmds.parent(ctr, grp)
+    cmds.parent(grp, prnt)
